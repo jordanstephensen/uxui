ef6a5d66219b77feb5cbf21e999a255d21d7a336
jordanstephensen <47726286+jordanstephensen@users.noreply.github.com>
2021-02-23 23:48:53 -0600

    update readme

--- src/pages/about.js
+++ src/pages/about.js
@@ -22,27 +22,27 @@ const About = () => (
                 <img className="header__logo" src={logo} alt="UXUI"/>
             </Link>
             <h4>
-                is a project intended to highlight free and open-source <br /> 
-                product design tools, assets, resources, and thier creators.
+              is a project intended to highlight free and open-source <br /> 
+              product design tools, assets, resources, and thier creators.
             </h4>
             <h5>
-            This site was built using Gatsby and hosted on Netlify. <br />
-                Fonts used are <a href="https://www.jetbrains.com/lp/mono/">Jetbrains Mono</a> and <a href="https://rsms.me/inter/">Inter</a> <br />
-                Build and deployment are triggered at every push to master. <br />
-                The site is currently in Beta and still a bit of a WIP. <br />
-                <a href="https://github.com/jordanstephensen/uxui">You are viewing v0.1.1</a>
+              Fonts used are <a href="https://www.jetbrains.com/lp/mono/">Jetbrains Mono</a> and <a href="https://rsms.me/inter/">Inter</a> <br />
+              This site was built using <a href="https://www.gatsbyjs.com/">Gatsby</a> and hosted on <a href="https://www.netlify.com/">Netlify</a>. <br />
+              Build and deployment are triggered at every push to master. <br />
+              The site is currently in Beta and still a bit of a WIP. <br />
+              <a href="https://github.com/jordanstephensen/uxui">You are viewing v0.1.1</a>
             </h5>
             <br />
             <h5>
-                Made by <br />
-                <a href="https://www.jordanstephensen.com">Jordan Stephensen</a>
+              Made by <br />
+              <a href="https://www.jordanstephensen.com">Jordan Stephensen</a>
             </h5>
             <a href="https://www.jordanstephensen.com">
-                <img className="about__logo-js" src={js} alt="Jordan Stephensen"/>
+              <img className="about__logo-js" src={js} alt="Jordan Stephensen"/>
             </a>
             <br />
             <h5>
-                (c) 2021
+              (c) 2021
             </h5>
             <br />
             <br />
