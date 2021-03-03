fb3b7adbcb3dc48d80e32faeeba6010f95716b4a
Jordan Stephensen <47726286+jordanstephensen@users.noreply.github.com>
2021-03-03 10:42:31 -0600

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -28,7 +28,7 @@ const About = () => (
               product design tools, assets, resources, and thier creators.
             </h4>
             <h5>
-              Fonts used are <a href="https://www.jetbrains.com/lp/mono/">Jetbrains Mono</a> and <a href="https://rsms.me/inter/">Inter</a> <br />
+              Fonts used are <a href="https://www.jetbrains.com/lp/mono/">Jetbrains Mono</a> and <a href="https://rsms.me/inter/">Inter</a>. <br />
               This site was built using <a href="https://www.gatsbyjs.com/">Gatsby</a> and is hosted on <a href="https://www.netlify.com/">Netlify</a>. <br />
               Build and deployment are triggered at every push to master. <br />
               The site is currently in Beta and still a bit of a WIP. <br />
